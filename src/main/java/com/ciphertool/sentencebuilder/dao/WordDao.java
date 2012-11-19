f641a3fdd14a485c1b3c1c50a81242612a60cf28
george <george@10.0.0.4>
2012-11-18 19:50:42 -0500

    Downgraded hibernate for compatibility with external apps (i.e. Grails) and made changes to let Spring do all the transaction and session management.

--- src/main/java/com/ciphertool/sentencebuilder/dao/WordDao.java
+++ src/main/java/com/ciphertool/sentencebuilder/dao/WordDao.java
@@ -23,9 +23,10 @@ import java.util.List;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.ciphertool.sentencebuilder.entities.Word;
 
@@ -39,13 +40,11 @@ public class WordDao {
 	 * This returns a list of all Words, so words will be duplicated if they
 	 * have multiple parts of speech.
 	 */
+	@Transactional(readOnly = true, propagation = Propagation.REQUIRES_NEW)
 	public List<Word> findAll() {
-		Session session = sessionFactory.openSession();
-		session.beginTransaction();
+		Session session = sessionFactory.getCurrentSession();
 		@SuppressWarnings("unchecked")
 		List<Word> result = (List<Word>) session.createQuery("from Word").list();
-		session.getTransaction().commit();
-		session.close();
 
 		return result;
 	}
@@ -53,69 +52,58 @@ public class WordDao {
 	/*
 	 * This returns a list of all unique Words, irrespective of parts of speech.
 	 */
+	@Transactional(readOnly = true, propagation = Propagation.REQUIRES_NEW)
 	public List<Word> findAllUniqueWords() {
-		Session session = sessionFactory.openSession();
-		session.beginTransaction();
+		Session session = sessionFactory.getCurrentSession();
 		@SuppressWarnings("unchecked")
 		List<Word> result = (List<Word>) session.createQuery(
 				"select distinct new Word(id.word, frequencyWeight) from Word").list();
-		session.getTransaction().commit();
-		session.close();
 
 		return result;
 	}
 
+	@Transactional(readOnly = true, propagation = Propagation.REQUIRES_NEW)
 	public List<Word> findByWordString(String word) {
 
-		Session session = sessionFactory.openSession();
-		session.beginTransaction();
+		Session session = sessionFactory.getCurrentSession();
 		@SuppressWarnings("unchecked")
 		List<Word> words = (List<Word>) session.createQuery(
 				"from Word where word = " + separator + wordParameter).setParameter(wordParameter,
 				word).list();
-		session.getTransaction().commit();
-		session.close();
 
 		return words;
 	}
 
+	@Transactional(propagation = Propagation.REQUIRES_NEW)
 	public boolean insert(Word w) {
-		Session session = sessionFactory.openSession();
-		session.beginTransaction();
+		Session session = sessionFactory.getCurrentSession();
 		session.save(w);
-		session.getTransaction().commit();
-		session.close();
 		return true;
 	}
 
+	@Transactional(propagation = Propagation.REQUIRES_NEW)
 	public boolean insertBatch(List<Word> wordBatch) {
-		Session session = sessionFactory.openSession();
-		Transaction transaction = session.beginTransaction();
+		Session session = sessionFactory.getCurrentSession();
 		for (Word word : wordBatch) {
 			session.save(word);
 		}
-		transaction.commit();
-		session.close();
 		return true;
 	}
 
+	@Transactional(propagation = Propagation.REQUIRES_NEW)
 	public boolean update(Word w) {
-		Session session = sessionFactory.openSession();
-		session.beginTransaction();
+		Session session = sessionFactory.getCurrentSession();
 		session.update(w);
-		session.getTransaction().commit();
-		session.close();
+
 		return true;
 	}
 
+	@Transactional(propagation = Propagation.REQUIRES_NEW)
 	public boolean updateBatch(List<Word> wordBatch) {
-		Session session = sessionFactory.openSession();
-		Transaction transaction = session.beginTransaction();
+		Session session = sessionFactory.getCurrentSession();
 		for (Word word : wordBatch) {
 			session.update(word);
 		}
-		transaction.commit();
-		session.close();
 		return true;
 	}
 
